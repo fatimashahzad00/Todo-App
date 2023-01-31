fd8c0deb2878dd28d26a1ff5ea20be952fc0fdb7
fatimashahzad00 <fatimashahzad062@gmail.com>
2023-01-31 19:10:37 +0500

    todo-app

---
+++ todoapp/app/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import CounterApp from "./counter-app";
+import CounterAppAdvanced from "./counter-app-advanced";
+import StrPrac from "./str-prac";
+import Todo from "./todo";
+
+export default function page() {
+  return(
+    <>
+    {/*<h1>String Practice and useState Practice</h1>
+    <StrPrac />
+    <br/>
+    <h1>Counter App</h1>
+    <CounterApp />
+  <h1>Counter App Advanced</h1>*/}
+    {/*<CounterAppAdvanced />*/}
+
+    <Todo />
+    </>
+  )
+}
